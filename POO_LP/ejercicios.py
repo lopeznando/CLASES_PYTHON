ddaef22a4dfc160d252df11c0e7f4224af44cbfb
Nando Lopez <nandolopezsantaria@gamil.com>
2023-10-03 11:42:30 -0500

    ejercicios de POO

--- POO_LP/ejercicios.py
+++ POO_LP/ejercicios.py
@@ -1,59 +1,105 @@
 
 #haciendo uso de la POO crear un objeto para 
 #la entidad celular
-class Celular:
-    marca='samsung'
-    version='año 2022'
+# class Celular:
+#     marca='samsung'
+#     version='año 2022'
 
-    def redes_sociales(self):
-        return 'hola, que haces'
-    def aplicaciones(self):
-        return 'camara,calculadora,etc'
+#     def redes_sociales(self):
+#         return 'hola, que haces'
+#     def aplicaciones(self):
+#         return 'camara,calculadora,etc'
     
-imprimir=Celular()
-print(imprimir.marca)
-print(imprimir.version)
-print(imprimir.redes_sociales())
-print(imprimir.aplicaciones())
-#vehiculo
-class Vehiculo:
-    marca='toyota'
-    año_fabricado='2020'
+# imprimir=Celular()
+# print(imprimir.marca)
+# print(imprimir.version)
+# print(imprimir.redes_sociales())
+# print(imprimir.aplicaciones())
+# #vehiculo
+# class Vehiculo:
+#     marca='toyota'
+#     año_fabricado='2020'
 
-    def velocidad(self):
-        return '100 kmh'
-    def capacidad_peso(self):
-        return '5 toneladas'
+#     def velocidad(self):
+#         return '100 kmh'
+#     def capacidad_peso(self):
+#         return '5 toneladas'
     
-resultado=Vehiculo()
-print(resultado.marca)
-print(resultado.año_fabricado)
-print(resultado.velocidad())
-print(resultado.capacidad_peso())
-#avion
-class Avion:
-    marca='ñus ñus'
-    forma='gavilan'
+# resultado=Vehiculo()
+# print(resultado.marca)
+# print(resultado.año_fabricado)
+# print(resultado.velocidad())
+# print(resultado.capacidad_peso())
+# #avion
+# class Avion:
+#     marca='ñus ñus'
+#     forma='gavilan'
 
-    def velocidad():
-        return '1000 kmh'
-    def capacidad_de_pasajeros():
-        return '200 personas'
+#     def velocidad(self):
+#         return '1000 kmh'
+#     def capacidad_de_pasajeros(self):
+#         return '200 personas'
     
-result=Avion()
-print(result.marca)
-print(result.forma)
-print(result.capacidad_de_pasajeros())
-print(result.velocidad())
-#freefire
-class Freefire:
-    personaje='negrito'
+# result=Avion()
+# print(result.marca)
+# print(result.forma)
+# print(result.capacidad_de_pasajeros())
+# print(result.velocidad())
+# #freefire
+# class Freefire:
+#     personaje='negrito'
 
-    def abilidades():
-        return 'saltar'
+#     def abilidades(self):
+#         return 'saltar'
 
-rest=Freefire()
-print(rest.personaje)
-print(rest.abilidades())
+# rest=Freefire()
+# print(rest.personaje)
+# print(rest.abilidades())
+#  #PC       
+# class Pc:
+#     marca='LG'
+#     caracteristica='rectangular'
 
-        
+#     def programas(self):
+#         return 'office','python'
+# nando=Pc()
+# print(nando.marca)
+# print(nando.caracteristica)
+# print(nando.programas())
+# #IMPRESORA
+# class Impresora:
+#     marca='Epson'
+#     funcionalidad='imprimir'
+
+#     def impresion(self):
+#         return 'blanco y negro','a color'
+# impri=Impresora()
+# print(impri.marca)
+# print(impri.funcionalidad)
+# print(impri.impresion())
+# #FACTURA
+
+class Factura:
+    nombre='nombre del comprador'
+    DNI='DNI del comprador'
+    def igv(self):
+        return ('agregar el igv')
+    def producto(self):
+        return ('agregar mas producto')  
+fac=Factura()
+print(fac.nombre)
+print(fac.DNI)
+print(fac.igv())
+print(fac.producto())
+    
+    
+
+
+    
+
+
+    
+    
+
+
+
